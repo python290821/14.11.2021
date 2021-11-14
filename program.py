40d6ff8ecf409c20ee2a1fa6119d285a49daf922
python290821 <90146835+python290821@users.noreply.github.com>
2021-11-14 19:47:04 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,3 +1,5 @@
+import math
+
 class Point():
     def __init__(self, x, y):
         self.x = x
@@ -28,14 +30,22 @@ class Point():
         return Point(self.x + other.x, self.y + other.y)
 
     def __repr__(self):
+        # default is class-name and memory address
         return f'Point({self.x}, {self.y})'
 
     def __str__(self):
+        # default is class-name and memory address
         return f'Point x:{self.x} y:{self.y}'
+
+    # Point(1,1) > (5,5)
+    def __gt__(self, other):
+        if type(other) != type(self):
+            return False
+        return math.sqrt(self.x**2 + self.y**2) > \
+               math.sqrt(other.x ** 2 + other.y ** 2)
 
 class Circle:
     pass
-
 
 p1 = Point(3.4, 12.1)
 p2 = Point(2.1, 2.22)
@@ -62,3 +72,5 @@ _p1 = Point(1, 3)
 _p2 = Point(5, 9.9)
 _lst = [_p1 , _p2]
 print(_lst)
+print('Point(1, 3) > Point(5, 9.9)',_p1 > _p2)
+print('Point(1, 3) > Point(5, 9.9)',_p1 < _p2)
